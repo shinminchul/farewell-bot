e4829edaa94c4f016655b6081766d2f9a18495ff
Minchul Shin <pillows@ajou.ac.kr>
2018-01-14 20:09:44 +0900

    make QnA class and barebone server

--- QnA.py
+++ QnA.py
@@ -1,12 +1,11 @@
 import requests
 
 class QnAmaker:
-    url = "https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/knowledgebases/"
     def __init__(self, kb_id, subscription_key):
-        self.kb_id=kb_id
-        self.subscription_key=subscription_key
+        self.url = 'https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/knowledgebases/' + kb_id
+        self.headers = {'Content-Type':'application/json; charset=utf-8', 'Ocp-Apim-Subscription-Key' : subscription_key}
     
-    def create(self, name, qnapairs, urls)
-        pass
-    
-    def delete(self)
+    def getAnswer(self, message):
+        generateanswer=self.url+"/generateAnswer"
+        r = requests.post(generateanswer, headers=self.headers, json={'question': message})
+        return r.json()
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
+import QnA
 import message
 
 
@@ -7,4 +8,34 @@ app = Flask(__name__)
 client = MongoClient()
 
 db = client["farewell"]
-collection, collection_clear = db.test, db.clear
+collection, collection_clear = db.bye, db.clear
+
+@app.route('/keyboard')
+def keyboard():
+    dataSend = {
+        "type": "buttons",
+        "buttons": ["똑똑"]
+    }
+    return jsonify(dataSend)
+
+
+@app.route('/message', methods=['POST'])
+def Message():
+    dataReceive = request.get_json()
+    user_key = dataReceive['user_key']  # user id get
+    content = dataReceive['content']    # user message get
+
+    #First Phase
+    try:
+        phase = getStatus(user_key, collection)["phase"]
+    except:
+        phase = 1
+
+    if phase == 1:
+        
+
+
+    return jsonify(dataSend)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
